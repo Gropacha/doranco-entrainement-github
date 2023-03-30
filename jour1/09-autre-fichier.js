867252d411169390fd4db2eaa25931b47911b3d6
Jacques-André BOURDOISEAU <jacquesandre.bourdoiseau@gmail.com>
2023-03-30 18:02:03 +0200

    fin de journée jeudi 30 mars

--- jour1/09-autre-fichier.js
+++ jour1/09-autre-fichier.js
@@ -4,3 +4,17 @@ const perso = {
     age: 30,
 
 }
+
+function calcul() {
+    return `${a.largeur} x ${a.hauteur} = ${a.largeur * a.hauteur}`
+}
+
+// git branch -M menu-principal
+// git checkout master
+// git branch -M main // renommer la branch master => main
+// git checkout menu-principal
+// git branch -M menu
+
+// git log --oneline
+
+// find de journée
